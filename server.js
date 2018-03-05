46f424ccba02f241d81c1455a8e4367aae92d361
bbaxa16 <brooke769@gmail.com>
2018-03-05 11:47:16 -0700

    added requirements to server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app     = express();
+const port    = process.env.PORT || 2080;
+
+
+//middleware
+app.use(express.static('public'));
+
+//listener
+app.listen(port, ()=> {
+  console.log('listening bruh');
+})
